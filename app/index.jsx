02d3f94d119772ffadc9e3fc535f17ed9afa29a0
yasuhamnn <156912831+yasuhamnn@users.noreply.github.com>
2026-05-23 19:31:02 +0800

    Add files via upload

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>WELCOME TO</Text>
+      <Text>KIDNEYCARE</Text>
+    </View>
+  )
+} 
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+})
